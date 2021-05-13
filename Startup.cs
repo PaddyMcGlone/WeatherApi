b27decdcb8196e5462572e83b782dc1bde313a33
Paddy McGlone <paddymcglone@gmail.com>
2021-05-13 20:04:47 +0100

    Adding WeatherAPI with service call.

---
+++ Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace WeatherApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddHttpClient<IWeatherService, WeatherService>(c =>
+            {
+                c.BaseAddress = new Uri("http://api.weatherapi.com/v1/current.json"); // Adding a named client.
+            }); 
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WeatherApi", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WeatherApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ WeatherService.cs
@@ -0,0 +1,32 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WeatherApi
+{
+    // Both in same file to save time
+    public interface IWeatherService
+    {
+        Task<string> Get(string cityName);
+    }
+
+    public class WeatherService : IWeatherService
+    {
+        private HttpClient _httpClient;
+
+        public WeatherService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> Get(string city)
+        {
+            var apiKey = "d6796d810e164f63a7b194048211205";
+
+            string apiUrl = $"?key={apiKey}&q={city}";
+
+            var response = await _httpClient.GetAsync(apiUrl);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
